d00fb3d1a35818bf93c591fecbce766c1253992b
wkyle251 <1155161938@link.cuhk.edu>
2024-05-12 04:21:50 +0800

    remove import

--- src/app/.well-known/pki-validation/[name]/route.js
+++ src/app/.well-known/pki-validation/[name]/route.js
@@ -1,11 +1,3 @@
-import { NextRequest, NextResponse } from "next/server";
-import { sign, verify } from "jsonwebtoken"
-import { cookies } from 'next/headers'
-import fs from "fs"
-import key from "../../../private"
-import db from '@/db_connection'
-import { ObjectId } from 'mongodb'
-import { v4 as uuidv4 } from 'uuid'
 
 export async function GET(request) {
     const file = fs.readFileSync(`8EE425EBE6FE38614632923008AAEE3C.txt`);
